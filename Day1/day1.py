2d1ba88a9ee934e1ca408edbdfa97db8314eab99
jayden fokkink <jaydenfokkink@gmail.com>
2022-04-06 23:05:39 +0200

    day1 completed

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,6 +1,12 @@
 count = 1
+count1 = 0  # added for part 2 ------
+count2 = 1  # added for part 2 ------
+count3 = 2  # added for part 2 ------
+count4 = 1   # added for part 2 ------
 answer = 0
+answer2 = 0  # added for part 2 ------
 intList = []
+sumList = []  # added for part 2 ------
 with open('day1PuzzleInput1') as file:
     lines = [line.rstrip('\n') for line in file]
     for line in lines:
@@ -11,6 +17,21 @@ with open('day1PuzzleInput1') as file:
             answer += 1
         if count < 1999:
             count += 1
-file.close()
+    # part 2 ------
+    for counter in intList:
+        sum1 = intList[count1] + intList[count2] + intList[count3]
+        sumList.append(sum1)
+        if count3 < 1999:
+            count1 += 1
+            count2 += 1
+            count3 += 1
+    for sum2 in sumList:
+        if sumList[count4] > sum2:
+            answer2 += 1
+        if count4 < 1999:
+            count4 += 1
 
 print(answer)
+print(answer2)  # added for part 2 ------
+
+file.close()
